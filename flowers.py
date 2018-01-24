1dfc69a72dc3fb18f67758b97ceeb08d2a78fbf2
noah <noahnaamad@gmail.com>
2018-01-23 21:37:45 -0800

    graphical analysis of sepal size

---
+++ flowers.py
@@ -0,0 +1,44 @@
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("flowers.csv", sep=",", header=None)
+data.columns =  ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)', 'species']
+
+print(data.head())
+
+
+print("Summary:")
+summary = data.describe()
+summary = summary.transpose()
+print(summary.head())
+
+
+### Sepal length plot:
+### sepal_length.png
+# plt.hist(data['sepal length (cm)'])
+# plt.title('Sepal Length')
+# plt.xlabel('height (cm)')
+# plt.ylabel('number')
+#plt.show()
+
+
+### Sepal Length vs Width
+seto = data[data['species'] == 'Iris-setosa']
+vers = data[data['species'] == 'Iris-versicolor']
+virg = data[data['species'] == 'Iris-virginica']
+
+x_axis = data['sepal length (cm)']
+y_axis = data['sepal width (cm)']
+colors = {'Iris-setosa': 'blue', 'Iris-versicolor': 'red', 'Iris-virginica': 'green'}
+se = plt.scatter(seto['sepal length (cm)'], seto['sepal width (cm)'], color = 'b')
+ve = plt.scatter(vers['sepal length (cm)'], vers['sepal width (cm)'], color = 'g')
+vi = plt.scatter(virg['sepal length (cm)'], virg['sepal width (cm)'], color = 'r')
+
+plt.legend((se, ve, vi), ('Setosa', 'Versicolor', 'Virginica'), scatterpoints=1)
+
+plt.title('Sepal Length vs Width')
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
+plt.show()
