168dbce3b9cdbbc5b4679b297ea1e8d006706216
noah <noahnaamad@gmail.com>
2018-03-10 14:39:49 -0800

    fix urls file

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 from collection import views
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='home')
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^admin/', include(admin.site.urls))
+]
