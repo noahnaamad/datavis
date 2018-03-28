743984ce42b16c908f9a19a785f7607228a70191
noah <noahnaamad@gmail.com>
2018-03-27 20:34:34 -0700

    add navigation and practice passing variables

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url, include
 
 from django.contrib import admin
 from collection import views
+from django.views.generic import TemplateView #added this
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
