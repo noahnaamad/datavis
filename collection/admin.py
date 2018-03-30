72d76021d2a0a4cee013e465ed57b2ba5f4194fb
noah <noahnaamad@gmail.com>
2018-03-30 16:29:15 -0700

    add dynamic data

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#import the model
+from collection.models import Post
+
+#set up automated slug creation
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+#register it
+admin.site.register(Post, PostAdmin)
